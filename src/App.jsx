8b97af917abe9fdac785420d20cba16280c73ea4
ro77en <lucas.mdantas@icloud.com>
2024-09-05 10:05:35 -0300

    responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import Converter from './components/Converter/Converter'
 
 function App() {
 
   return (
     <>
-    <div className="title">
-      <h1>PDF 2 AUDIO</h1>
+    <div className="app-container">
+      <div className="title">
+        <h1>PDF 2 AUDIO</h1>
+      </div>
+      <div className="wrapper">
+        <Converter />
+      </div>
+      <div className="footer">
+        ro77en
+      </div>
     </div>
+
     </>
   )
 }
