a6ef05d27a137471956f359d94f6c69d1c5a8d26
ro77en <lucas.mdantas@icloud.com>
2024-09-05 21:56:40 -0300

    file selection handler

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./Converter.css";
-import { useRef } from "react";
 import Message from "../Message/Message";
 
 function Converter() {
@@ -8,30 +7,30 @@ function Converter() {
   const [showMessage, setShowMessage] = useState(false);
   const inputRef = useRef();
 
-  const handleDragOver = (event) => {
-    event.preventDefault();
-  }
+  const handleFileSelection = (file) => {
+    file && file.type === "application/pdf"
+      ? setFile(file)
+      : setShowMessage(true);
+  };
 
   const handleDrop = (event) => {
     event.preventDefault();
     const files = event.dataTransfer.files;
-    files.length > 1 ? setShowMessage(true) : setFile(files[0])
-    console.log(files[0])
-  }
-  
+    handleFileSelection(files[0]);
+  };
+
   const handleOnChange = (event) => {
     const files = event.target.files;
-    files.length > 1 ? setShowMessage(true) : setFile(files[0])
-    console.log(files[0])
-  }
-  
+    handleFileSelection(files[0]);
+  };
+
   const handleBrowseFile = () => {
-    inputRef.current.click()
-  }
+    inputRef.current.click();
+  };
 
   const hideMessage = () => {
     setShowMessage(false);
-  }
+  };
 
   return (
     <>
@@ -39,14 +38,23 @@ function Converter() {
         {!file && (
           <div
             className="dropzone"
-            onDragOver={handleDragOver}
+            onDragOver={(event) => event.preventDefault()}
             onDrop={handleDrop}
           >
             <img src="src\assets\add-pdf.png" alt="add pdf icon" />
             <h2>Drag & Drop your PDF File</h2>
             <p>or</p>
-            <input type="file" name="file" id="file" hidden onChange={handleOnChange} ref={inputRef} />
-            <button onClick={handleBrowseFile}>{file ? 'Convert' : 'Browse Files'}</button>
+            <input
+              type="file"
+              name="file"
+              id="file"
+              hidden
+              onChange={handleOnChange}
+              ref={inputRef}
+            />
+            <button onClick={handleBrowseFile}>
+              {file ? "Convert" : "Browse Files"}
+            </button>
           </div>
         )}
       </div>
