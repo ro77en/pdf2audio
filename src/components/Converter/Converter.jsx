efb929c0e06c6bb2752d084f500f214a54870050
ro77en <lucas.mdantas@icloud.com>
2024-09-05 21:04:35 -0300

    handle file select logic

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -1,16 +1,56 @@
+import { useState } from "react";
 import "./Converter.css";
+import { useRef } from "react";
+import Message from "../Message/Message";
 
 function Converter() {
+  const [file, setFile] = useState(null);
+  const [showMessage, setShowMessage] = useState(false);
+  const inputRef = useRef();
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  }
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const files = event.dataTransfer.files;
+    files.length > 1 ? setShowMessage(true) : setFile(files[0])
+    console.log(files[0])
+  }
+  
+  const handleOnChange = (event) => {
+    const files = event.target.files;
+    files.length > 1 ? setShowMessage(true) : setFile(files[0])
+    console.log(files[0])
+  }
+  
+  const handleBrowseFile = () => {
+    inputRef.current.click()
+  }
+
+  const hideMessage = () => {
+    setShowMessage(false);
+  }
+
   return (
     <>
       <div className="converter">
-        <div className="dropzone">
-          <img src="src\assets\add-pdf.png" alt="" />
-          <h2>Drag & Drop your PDF File</h2>
-          <p>or</p>
-          <button>Browse File</button>
-        </div>
+        {!file && (
+          <div
+            className="dropzone"
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            <img src="src\assets\add-pdf.png" alt="add pdf icon" />
+            <h2>Drag & Drop your PDF File</h2>
+            <p>or</p>
+            <input type="file" name="file" id="file" hidden onChange={handleOnChange} ref={inputRef} />
+            <button onClick={handleBrowseFile}>{file ? 'Convert' : 'Browse Files'}</button>
+          </div>
+        )}
       </div>
+      <Message showMessage={showMessage} hideMessage={hideMessage} />
     </>
   );
 }
