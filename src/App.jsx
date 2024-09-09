0b7c8251741843acc7b6000a6b177058efe771ba
ro77en <lucas.mdantas@icloud.com>
2024-09-09 09:18:25 -0300

    useContext for file management

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,27 @@
-import './App.css'
-import Converter from './components/Converter/Converter'
+import { useState, createContext } from "react";
+import "./App.css";
+import Converter from "./components/Converter/Converter";
+
+export const FileContext = createContext();
 
 function App() {
+  const [file, setFile] = useState(null);
 
   return (
     <>
-    <div className="app-container">
-      <div className="title">
-        <h1>PDF 2 AUDIO</h1>
-      </div>
-      <div className="wrapper">
-        <Converter />
-      </div>
-      <div className="footer">
-        ro77en
-      </div>
-    </div>
-
+      <FileContext.Provider value={{file, setFile}}>
+        <div className="app-container">
+          <div className="title">
+            <h1>PDF 2 AUDIO</h1>
+          </div>
+          <div className="wrapper">
+            <Converter />
+          </div>
+          <div className="footer">ro77en</div>
+        </div>
+      </FileContext.Provider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
